f72285e918a7a428b446a8f870fbb86e4038eb99
Amjad-722 <aliamjad722ee@gmail.com>
2024-07-05 17:07:50 +0500

    added new files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-export default function App() {
+import React from 'react';
+import './App.css';
+
+import Button from './components/Button/Button';
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="App container px-2">
+      <Button />
+    </div>
+  );
 }
+
+export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+
+const loremIpsumText = [
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+];
+
+const Button = () => {
+  const [count, setCount] = useState(1);
+  const [type, setType] = useState('paragraphs');
+  const [output, setOutput] = useState('');
+
+  const handleGenerate = () => {
+    let generatedText = '';
+
+    switch (type) {
+      case 'paragraphs':
+        for (let i = 0; i < count; i++) {
+          generatedText += `<p>${loremIpsumText.join(' ')}</p>`;
+        }
+        break;
+      case 'sentences':
+        for (let i = 0; i < count; i++) {
+          generatedText += `${loremIpsumText[i % loremIpsumText.length]} `;
+        }
+        break;
+      case 'words':
+        const words = loremIpsumText.join(' ').split(' ');
+        for (let i = 0; i < count; i++) {
+          generatedText += `${words[i % words.length]} `;
+        }
+        break;
+      default:
+        break;
+    }
+
+    setOutput(generatedText);
+  };
+
+  return (
+    <div className="container mx-auto my-8 p-4 max-w-lg bg-gray-200 border border-gray-400 rounded-lg">
+      <h1 className="text-center mb-4 text-xl font-bold">Lorem Ipsum Generator</h1>
+      <div className="flex flex-col space-y-4">
+        <input
+          className="py-2 px-3 border border-red-700 rounded-lg bg-custom-red"
+          type="number"
+          id="count"
+          name="count"
+          min="1"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <select
+          className="py-2 px-3 border border-red-700 rounded-lg bg-custom-red"
+          id="type"
+          name="type"
+          value={type}
+          onChange={(e) => setType(e.target.value)}
+        >
+          <option value="paragraphs">Paragraphs</option>
+          <option value="sentences">Sentences</option>
+          <option value="words">Words</option>
+        </select>
+        <button className="px-4 py-2 border border-red-700 rounded-lg bg-custom-red" onClick={handleGenerate}>
+          Generate!
+        </button>
+      </div>
+      <div className="mt-4" dangerouslySetInnerHTML={{ __html: output }}></div>
+    </div>
+  );
+};
+
+export default Button;
